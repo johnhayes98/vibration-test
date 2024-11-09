1f3ad5ef272a180112705799827a0356315e3816
johnhayes98 <97008104+johnhayes98@users.noreply.github.com>
2024-11-08 19:10:41 -0600

    added PWM to nano code and tuned vibration_patterns

--- vibration_patterns.py
+++ vibration_patterns.py
@@ -3,24 +3,29 @@ This file contains the vibration patterns. Each vibration is formatted as a list
 Each list contains two values: [duration_in_ms, intensity]. 
 Both should be a single byte ranging from 0 to 255.
 
+For 5v applied to motor, pwm of 100 seems to be barely detectable. Use range 100-255
+
 '''
 
 iPhone = {
-    "iMessage": [[255, 255], [50, 0], [255, 255], [0, 0]],
-    "Email": [[135,200], [0,0]],
-    "WhatsApp": [[100,200], [0,0]],
+    "iMessage": [[255, 255], [75, 0], [150, 225], [100, 200], [0, 0]],
+    "Email": [[255,255], [0,0]],
+    "WhatsApp": [[150,255], [0,0]],
     "None": [[0,0]]
 }
 
 Android = {
-    "Text" : [],
-    "Email" : [],
-    "WhatsApp" : [],
-    "SnapChat" : []
+    "Text" : [[255,100], [0, 0]],
+    "Email" : [[255,150], [0, 0]],
+    "WhatsApp" : [[255,200], [0, 0]],
+    "SnapChat" : [[255,255], [0, 0]]
 }
 
-test = {
-
+PWM_Ranges = {
+    "100" : [[255,100],[255,100],[255,100],[255,100], [0, 0]],
+    "150" : [[255,150],[255,150],[255,150],[255,150], [0, 0]],
+    "200" : [[255,200],[255,200],[255,200],[255,200], [0, 0]],
+    "255" : [[255,255],[255,255],[255,255],[255,255], [0, 0]]
 }
 
 test2 = {
